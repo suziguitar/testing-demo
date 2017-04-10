cd0b381bc6e76169dda7176fa425425db9bd086b
Suzanne Ross <suzanneross@Suzannes-MacBook-Pro.local>
2017-04-10 10:07:20 +0100

    adding lm

--- somecode.R
+++ somecode.R
@@ -3,3 +3,6 @@ df <- read.table('http://data.bris.ac.uk/datasets/swyt56qr4vaj17op9cw3sag7d/Lsky
               header=TRUE,sep=',')
 head(df)
 
+# R code
+lm.result=lm(conspiracist_avg~age,data=df)
+summary(lm.result)
